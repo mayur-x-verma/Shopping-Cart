2d239884348b72654b13c8da8a6a54986241e4b5
Mayur Verma <mayurverma619@gmail.com>
2023-10-23 17:38:41 +0530

    committing the final changes

---
+++ src/product.js
@@ -0,0 +1,66 @@
+import product1 from "./assets/ts1.webp";
+import product2 from "./assets/ts2.jpg";
+import product3 from "./assets/ts12.jpg";
+import product4 from "./assets/ts11.jpg";
+import product5 from "./assets/ts5.jpg";
+import product6 from "./assets/ts10.jpeg";
+import product7 from "./assets/ts7.jpg";
+import product8 from "./assets/ts8.png";
+import product9 from "./assets/ts9.webp";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Itachi",
+    price: 19.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "E S A R",
+    price: 21.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "Uzumaki Naruto",
+    price: 11.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Satoru Gojo",
+    price: 28.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Dark Light ",
+    price: 19.99,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "SSJ Goku",
+    price: 68.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Tanjiro",
+    price: 20.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Naruto",
+    price: 40.0,
+    productImage: product8,
+  },
+  {
+    id: 9,
+    productName: "Snake eye",
+    price: 24.0,
+    productImage: product9,
+  },
+];
